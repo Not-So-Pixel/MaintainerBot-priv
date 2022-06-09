bd82abe14f5133fdf8fd606441aa0a296f0cf6a2
Ashwin.D.S <astroashwin@outlook.com>
2022-06-09 19:51:15 +0000

    ok shi works

--- MaintainerBot/samply.py
+++ MaintainerBot/samply.py
@@ -1,16 +1,29 @@
-import requests
+# import requests
 
-url = "https://api.github.com/repos/PixelOS-Releases/releases/actions/workflows/17046064/dispatches"
+# url = "https://api.github.com/repos/PixelOS-Releases/releases/actions/workflows/17046064/dispatches"
 
-token = "token actualtoken"
+# token = "token actualtoken"
 
-# https://api.github.com/repos/OWNER/REPO/actions/workflows
+# # https://api.github.com/repos/OWNER/REPO/actions/workflows
 
-DataStuff = {
-    'Accept': 'application/vnd.github.v3+json',
-    "Authorization" : token,
-}
+# DataStuff = {
+#     'Accept': 'application/vnd.github.v3+json',
+#     "Authorization" : token,
+# }
 
-a = requests.get(url, headers=DataStuff)
+# a = requests.get(url, headers=DataStuff)
 
-print(a.content)
+# print(a.content)
+
+import os
+
+commands = [
+#     "curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg",
+# "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main\" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null",
+# "sudo apt update",
+# "sudo apt install gh",
+    "cd releases && echo $(pwd) && gh workflow run 17046064"
+]
+
+for command in commands:
+    os.system(command)
